4f6704dffd1ecc472c6db466f17ad00396022066
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-14 12:51:41 +0900

    Post Model 생성

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+
+
+
+admin.site.register(Post,PostAdmin)
